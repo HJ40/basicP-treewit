1c2af080d76624b4f9c94e1c79734dd94aeaafdb
Hajimari <221500826+HJ40@users.noreply.github.com.>
2025-07-23 12:27:34 +0700

    up date D3

---
+++ Class3.py
@@ -0,0 +1,27 @@
+# hasRice = True
+# hasSpoon = False
+
+# #if hasRice:
+# print("มีข้าว")
+# if hasSpoon:
+#     print("มีช้อน")
+#     print("รับประทาน")   
+# else :
+#     print("รับประทานไม่ได้")    
+# #else:   
+# print("อดรับประทาน")        
+# 5
+# # โปรแกรมคำนวณผลรวม
+
+# int(input("จำนวนวนรอบ  " ))
+# box = 0
+# for i in range(1,6): 
+#      box +=i 
+#      print("รอบที่ ", i) 
+#      print("ผลรวมคือ ",box  )
+# print("ค่าbox ", box)
+
+i = 0
+while i > 5:
+    i += 1
+    print(i)
